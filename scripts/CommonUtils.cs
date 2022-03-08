3719487bc0bad7caa21ddcd9b92507c2eed586e9
MrTK-Dev <Dev.MrTK@gmail.com>
2022-03-08 14:08:55 +0100

    add documentation for CommonUtils & Constants

--- scripts/CommonUtils.cs
+++ scripts/CommonUtils.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace DoYouAssignment.scripts
 {
+    /// <summary>
+    /// This class holds common functions that do not belong to a specific part of the application.
+    /// </summary>
     public static class CommonUtils
     {
-        public static void OpenLink(string link)
+        /// <summary>
+        /// This opens the webpage from the given URL with the standard browser.
+        /// </summary>
+        /// <param name="url">The URL of the webpage.</param>
+        public static void OpenLink(string url)
         {
-            System.Diagnostics.Process.Start(link);
+            System.Diagnostics.Process.Start(url);
         }
     }
 }
--- scripts/Constants.cs
+++ scripts/Constants.cs
@@ -6,12 +6,29 @@ using System.Threading.Tasks;
 
 namespace DoYouAssignment.scripts
 {
+    /// <summary>
+    /// This class holds classes with different constants.
+    /// </summary>
     public static class Constants
     {
+        /// <summary>
+        /// This class holds all links to webpages.
+        /// </summary>
         public static class Links
         {
+            /// <summary>
+            /// This is the link to the repository on GitHub.
+            /// </summary>
             public static string github = "https://github.com/DoYouCircle/DoYouAssignment";
+
+            /// <summary>
+            /// This is the link to our GitHub profile.
+            /// </summary>
             public static string author = "https://github.com/DoYouCircle";
+
+            /// <summary>
+            /// This is the link to the issues page on GitHub.
+            /// </summary>
             public static string issues = "https://github.com/DoYouCircle/DoYouAssignment/issues";
         }
     }
