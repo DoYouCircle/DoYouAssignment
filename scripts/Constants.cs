3fdcef899c73529c2b19d8cfd1eddced2bbae891
MrTK-Dev <Dev.MrTK@gmail.com>
2022-03-08 13:52:46 +0100

    add class for constants and folder for scripts

---
+++ scripts/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoYouAssignment.scripts
+{
+    public static class Constants
+    {
+        public static class Links
+        {
+            public static string github = "https://github.com/DoYouCircle/DoYouAssignment";
+            public static string author = "https://github.com/DoYouCircle";
+            public static string issues = "https://github.com/DoYouCircle/DoYouAssignment/issues";
+        }
+    }
+}
