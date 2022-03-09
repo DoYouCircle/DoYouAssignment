90227c1517a916fd67a0ee98c64959875f16937b
MrTK-Dev <Dev.MrTK@gmail.com>
2022-03-09 13:29:33 +0100

    add notifier functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static DoYouAssignment.scripts.CommonUtils;
 
 namespace DoYouAssignment
 {
@@ -25,6 +26,9 @@ namespace DoYouAssignment
         public MainWindow()
         {
             InitializeComponent();
+
+            Notifier.Throw(Notifier.TYPE.WARNING, "Your house could burn!");
+            Notifier.Throw(Notifier.TYPE.ERROR, "Your house is currently burning!");
         }
 
         private void GITHUB_Click(object sender, RoutedEventArgs e)
@@ -50,6 +54,31 @@ namespace DoYouAssignment
         private void COURSEVIEW_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.Content = new CourseView();
+        }
+
+
+        private void NOTIFIER_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (CommonUtils.Notifier.Notes.Count > 0)
+                CommonUtils.Notifier.Notes[Notifier.activeIndex].Remove();
+        }
+
+        public void NOTIFIER_Update(Notifier.Note activeNote)
+        {
+            if (activeNote == null)
+            {
+                NOTIFIER_Type.Text = "";
+
+                NOTIFIER_Message.Text = "";
+
+                return;
+            }
+
+            NOTIFIER_Type.Foreground = activeNote.GetColor();
+
+            NOTIFIER_Type.Text = activeNote.GetType_();
+
+            NOTIFIER_Message.Text = activeNote.Message;
         }
     }
 }
--- scripts/CommonUtils.cs
+++ scripts/CommonUtils.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace DoYouAssignment.scripts
 {
@@ -11,6 +13,8 @@ namespace DoYouAssignment.scripts
     /// </summary>
     public static class CommonUtils
     {
+        private static readonly MainWindow mw = (MainWindow)Application.Current.MainWindow;
+
         /// <summary>
         /// This opens the webpage from the given URL with the standard browser.
         /// </summary>
@@ -18,6 +22,96 @@ namespace DoYouAssignment.scripts
         public static void OpenLink(string url)
         {
             System.Diagnostics.Process.Start(url);
+        }
+
+        public class Notifier
+        {
+            public enum TYPE
+            {
+                INFO,
+                WARNING,
+                ERROR
+            }
+
+            public class Note
+            {
+                public TYPE Type { get; set; }
+                public string Message { get; set;  }
+
+                public void Add()
+                {
+                    Notes.Add(this);
+
+                    activeIndex = Notes.Count - 1;
+
+                    mw.NOTIFIER_Update(this);
+                }
+
+                public void Remove()
+                {
+                    Notes.Remove(this);
+                    
+                    Update();
+                }
+
+                public Brush GetColor()
+                {
+                    switch (Type)
+                    {
+                        case TYPE.INFO:
+                            return Brushes.Black;
+                        case TYPE.WARNING:
+                            return Brushes.Orange;
+                        case TYPE.ERROR:
+                            return Brushes.Red;
+                        default:
+                            return Brushes.Black;
+                    }
+                }
+
+                public string GetType_()
+                {
+                    switch (Type)
+                    {
+                        case TYPE.INFO:
+                            return "INFO";
+                        case TYPE.WARNING:
+                            return "WARNING";
+                        case TYPE.ERROR:
+                            return "ERROR";
+                        default:
+                            return "";
+                    }
+                }
+            }
+
+            public static List<Note> Notes = new List<Note>();
+
+            public static int activeIndex = 0;
+
+            /// <summary>
+            /// Will be called from different places.
+            /// </summary>
+            /// <param name="Type"></param>
+            /// <param name="Message"></param>
+            public static void Throw(TYPE Type, string Message)
+            {
+                new Note() {
+                    Type = Type,
+                    Message = Message
+                }.Add();
+            }
+
+            public static void Update()
+            {
+                activeIndex = Notes.Count - 1;
+
+                if (Notes.Count > 0)
+                    mw.NOTIFIER_Update(Notes[activeIndex]);
+
+                else
+                    mw.NOTIFIER_Update(null);
+            }
         }
     }
 }
