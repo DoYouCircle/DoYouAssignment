16f0088b6bb12a57cb6331c64a670d11df95aef7
MrTK-Dev <Dev.MrTK@gmail.com>
2022-03-14 20:36:10 +0100

    dirty code for CourseView - be aware of danger!

--- pages/CourseView.xaml.cs
+++ pages/CourseView.xaml.cs
@@ -23,6 +23,65 @@ namespace DoYouAssignment.pages
         public CourseView()
         {
             InitializeComponent();
+
+            // Test Data for ui design
+            DG_COURSE.DataContext = new List<Test>()
+            {
+                new Test()
+                {
+                    NAME = "Mathe",
+                    PERCENTAGE = 0.5f
+                },
+                new Test()
+                {
+                    NAME = "Physik",
+                    PERCENTAGE = 0f
+                },
+                new Test()
+                {
+                    NAME = "Informatik",
+                    PERCENTAGE = 0.25f 
+                }
+            };
+
+        }
+
+        public class Test
+        {
+            public string NAME { get; set; }
+
+            public float PERCENTAGE { get; set; }
+        }
+
+        public class TestGroups
+        {
+            public string NAME { get; set; }
+
+            public string Curse { get; set; }
+        }
+
+        public List<TestGroups> TestGroupsList = new List<TestGroups>()
+        {
+            new TestGroups() { Curse = "Mathe", NAME = "ein Mathekurs"},
+            new TestGroups() { Curse = "Mathe", NAME = "ein weiterer Mathekurs"},
+            new TestGroups() { Curse = "Informatik", NAME = "ein Kurs der Informatik"}
+        };
+
+        private void DG_COURSE_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Test testcourse = (Test)DG_COURSE.SelectedItem;
+
+            List<TestGroups> groups = new List<TestGroups>();
+
+            foreach (var item in TestGroupsList)
+            {
+                if (item.Curse == testcourse.NAME)
+                {
+                    groups.Add(item);
+                }
+            }
+
+            DG_AGROUP.DataContext = groups;
         }
     }
 }
